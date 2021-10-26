03afe431a4672b8c5de8115f248c7b222dfddecf
Shae Allison <sallison@SAllison-MBP.local>
2021-10-26 12:34:37 -0400

    refactoring results to be functional component instead of class

--- src/screens/results/index.js
+++ src/screens/results/index.js
@@ -1,73 +1,39 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
+import {React, useState, useEffect} from 'react'
 
-import Detail from '../detail'
+const Results = () => {
+  const [error, setError] = useState()
+  const [isLoaded, setLoader] = useState(false)
+  const [heros, setHeros] = useState([])
 
-class Results extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      error: null,
-      isLoaded: false,
-      heros: []
-    }
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch('/all.json')
       .then(res => res.json())
       .then(
         (result) => {
-          this.setState({
-            isLoaded: true,
-            heros: result
-          })
+          setLoader(true)
+          setHeros(result)
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          })
+          setLoader(true)
+          setError(error)
         }
       )
-  }
+  })
 
-  render() {
-    const { error, isLoaded, heros } = this.state
-    if (error) {
-      return <div>Error: {error.message}</div>
-    } else if (!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <>
-          {/* <Router>
-            <Link to="/detail">Detail</Link>
-            <Switch>
-              <Route path="/detail">
-                <Detail />
-              </Route>
-            </Switch>
-          </Router> */}
-          <ul>
-            {heros.map(hero => (
-              <li key={hero.id}>
-                {hero.name}
-              </li>
-            ))}
-          </ul>
-        </>
-      )
-    }
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <ul>
+        {heros.map(hero => (
+          <li key={hero.id}>
+            {hero.name}
+          </li>
+        ))}
+      </ul>
+    )
   }
 }
 
